d90b0ee848fbfd4bd7866504d9c65fdc9520cf6d
권준희 <june@gwonjunhuiui-MacBookAir.local>
2023-01-05 21:40:59 +0900

    throw catch

--- swift.playground/Contents.swift
+++ swift.playground/Contents.swift
@@ -1,21 +1,40 @@
 import UIKit
+//
+//enum DivideError:Error{
+//    case cannotzero
+//}
+//
+//func thrFunction(dividend: Int, divisor:Int) throws -> Int{
+//    if divisor==0{
+//        throw DivideError.cannotzero
+//    }
+//    return (dividend/divisor)
+//}
+//
+//
+//do{
+//    //let result = try thrFunction(dividend: 80, divisor: 6)
+//    let result = try thrFunction(dividend:80, divisor:0)
+//    print(result)
+//}catch{
+//    print(error.localizedDescription)
+//}
 
-enum DivideError:Error{
+
+enum errrror: Error{
     case cannotzero
 }
 
-func thrFunction(dividend: Int, divisor:Int) throws -> Int{
-    if divisor==0{
-        throw DivideError.cannotzero
+func divideSomething (dividend:Int, divisor:Int) throws -> Int{
+    if (divisor==0) {
+        throw errrror.cannotzero
     }
-    return (dividend/divisor)
+    return dividend/divisor
 }
 
-
 do{
-    //let result = try thrFunction(dividend: 80, divisor: 6)
-    let result = try thrFunction(dividend:80, divisor:0)
-    print(result)
+    let result = try divideSomething(dividend: 80, divisor: 10)
 }catch{
     print(error.localizedDescription)
 }
+
